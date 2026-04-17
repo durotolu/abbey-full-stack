d5233a83847129e31aa2f4aece5264f4234bf3cb
Modurotolu Olokode <50303829+durotolu@users.noreply.github.com>
2026-04-17 22:23:27 +0100

    connected services to main app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import cors from "cors";
+import authRoutes from "./routes/auth.routes";
+import followRoutes from "./routes/follow.routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/auth", authRoutes);
+app.use("/follow", followRoutes);
 
 export default app;
